57d275fedd8dd23904a48b809f49f09eb595bcb4
Kevin Mittman <mittman@users.noreply.github.com>
2015-04-27 14:24:01 -0700

    Move output to print function

--- parser.cpp
+++ parser.cpp
@@ -34,6 +34,12 @@ string const getIO(const string column9) {
 	}
 }
 
+void const printCommand(const string cycle, const int words, const int num, const string direction) {
+	cout << "Line " << num << ". ";
+	cout << cycle << " " << direction << " command: ";
+	cout << words << " words" << endl;
+}
+
 int main(int argc, char* argv[]) {
 
 	// Check parameters
@@ -50,6 +56,8 @@ int main(int argc, char* argv[]) {
 
 		// Line by line
 		while (getline(f, line)) {
+			int words = 0;
+			string cycle = "";
 
 			// Split into vector by whitespace
 			istringstream linestream(line);
@@ -60,15 +68,23 @@ int main(int argc, char* argv[]) {
 
 			// S-to-D marker
 			if(column[6] == "40000810") {
-				cout << column[6] << "\t" << column[7] << "\t Line " << num << ". " << "\t";
-				cout << getIO(column[9]) << " S-to-D command: ";
-				cout << getLength(column[7]) << " words" << endl;
+				cout << column[6] << "\t" << column[7] << "\t"; // debugging
+				words = getLength(column[7]);
+				cycle = getIO(column[9]);
+				printCommand(cycle, words, num, "S-to-D");
+				if(words > 0) {
+					// do stuff
+				}
 			}
 			// D-to-S marker
 			else if(column[6] == "40000C18") {
-				cout << column[6] << "\t" << column[7] << "\t Line " << num << ". " << "\t";
-				cout << getIO(column[9]) << " D-to-S command: ";
-				cout << getLength(column[7]) << " words" << endl;
+				cout << column[6] << "\t" << column[7] << "\t"; // debugging
+				words = getLength(column[7]);
+				cycle = getIO(column[9]);
+				printCommand(cycle, words, num, "S-to-D");
+				if(words > 0) {
+					// do stuff
+				}
 			}
 
 			++num;
