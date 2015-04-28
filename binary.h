3ffe91c333d035913a64857cf7283ad62fe46171
Hao Nguyen <howietnguyen@gmail.com>
2015-04-28 04:50:56 -0700

    headers for binary.cpp

---
+++ binary.h
@@ -0,0 +1,9 @@
+#include <isostream>
+#include <string>
+#include <math>
+using namespace std;
+
+void displayBinary(int number); // Display the binary
+unsigned int shiftBinary(int n, int numOfShift, unsigned int bitMask); //Shifts the bit
+string binaryCode(unsigned int a, int word); //Shows the description provided
+unsigned int bitSwap(unsigned int n); // swaps the bit for high to low
